98690a0ec4ec97be2c2cfa01bd900f962b367135
Your Name <you@example.com>
2019-02-03 15:52:17 +0100

    add CustomListView

--- app/src/main/java/e/brucelee/shop/Controller/MainActivity.kt
+++ app/src/main/java/e/brucelee/shop/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package e.brucelee.shop.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import e.brucelee.shop.Model.Category
 import e.brucelee.shop.R
+import e.brucelee.shop.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/e/brucelee/shop/Model/Category.kt
+++ app/src/main/java/e/brucelee/shop/Model/Category.kt
@@ -1,3 +1,7 @@
 package e.brucelee.shop.Model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
